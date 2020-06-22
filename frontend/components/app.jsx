068336aa6e54d2ed1a2704e5c39393bc8d70aa01
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-22 16:34:59 -0700

    Added a stub landing page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Signup from "./session/signup";
 import Login from "./session/login";
 import { AuthRoute } from "../utils/route_util";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  return <HashRouter>
-    <AuthRoute path="/signup" component={Signup} />
-    <AuthRoute path="/login" component={Login} />
-  </HashRouter>
+
+  const usernameSelector = state => state.session.user.username;
+
+  const username = useSelector(usernameSelector);
+
+  return <div className="main">
+    <HashRouter>
+      <Switch>
+        <Route path="/">
+          <h1>Hello, { username ? username : "Guest" }</h1>
+        </Route>
+        <AuthRoute path="/signup" component={Signup} />
+        <AuthRoute path="/login" component={Login} />
+      </Switch>
+    </HashRouter>
+  </div>
 }
 
 export default App;
