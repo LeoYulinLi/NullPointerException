2abbb726f66372fefb1d6dc3b39f0dd03025aeb4
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-22 12:19:33 -0700

    Added front end hello world

---
+++ frontend/index.jsx
@@ -0,0 +1,7 @@
+import Root from "./components/root";
+import ReactDOM from "react-dom";
+import React from "react";
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Root />, document.getElementById("root"));
+})
