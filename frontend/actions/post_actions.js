b9ad1f7b1c454ec92f1c8a6a8fe4332b03e2aa78
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-23 14:25:00 -0700

    Some initial question listing work on the front end

--- frontend/actions/post_actions.js
+++ frontend/actions/post_actions.js
@@ -1,6 +1,51 @@
 import { postQuestion } from "../utils/api_utlis";
 import { refreshSession } from "./session_actions";
+import React from "react";
+import { Action } from "redux";
 
+export const RECEIVE_REVISIONS = 'RECEIVE_REVISIONS'
+
+/**
+ * @typedef Revision
+ * @property {number} id
+ * @property {string} title
+ * @property {string} body
+ * @property {string} note
+ * @property {number} post_id
+ * @property {number} user_id
+ * @property {string} create_at
+ */
+
+/**
+ * @typedef {Action<RECEIVE_REVISIONS>} ReceiveRevisionsAction
+ * @property {{[number]: Revision}}revisions
+ */
+
+
+/**
+ * @param {{[number]: Revision}} revisions
+ * @return {ReceiveRevisionsAction}
+ */
+function receiveRevisions(revisions) {
+  return {
+    type: RECEIVE_REVISIONS,
+    revisions
+  }
+}
+
+/**
+ * @typedef QuestionSummary
+ * @property {Revision} revision
+ * @property
+ */
+
+/**
+ *
+ * @param question
+ */
+function receiveQuestion(question) {
+
+}
 
 export function askQuestion(question) {
   return function (dispatch) {
