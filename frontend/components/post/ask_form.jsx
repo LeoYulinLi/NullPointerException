b40ce2761e5a409cd27d2d8c68d49c3b23a28a9e
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-25 10:31:55 -0700

    Improved ask form

--- frontend/components/post/ask_form.jsx
+++ frontend/components/post/ask_form.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { askQuestion } from "../../actions/post_actions";
+import ReactMarkdown from "react-markdown";
+import { EditorHint } from "./widgets";
 
 /**
  * @typedef AskForm
@@ -31,21 +33,32 @@ const AskForm = () => {
     dispatch(askQuestion({ title, body }));
   }
 
+  useEffect(() => {
+    const $root = $('#root');
+    $root.addClass("ask-page");
+    return () => $root.removeClass("ask-page");
+  }, [])
+
   return <div className="post-page">
+    <h1>Ask a public question</h1>
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="title">
-          <strong>Title</strong>
-          <small>Be specific and imagine you’re asking a question to another person</small>
-        </label>
-        <input id="title" type="text" value={ title } onChange={ event => setTitle(event.target.value) }/>
-      </div>
-      <div className="form-group">
-        <label htmlFor="body">
-          <strong>Body</strong>
-          <small>Include all the information someone would need to answer your question</small>
-        </label>
-        <textarea id="body" value={ body } onChange={ event => setBody(event.target.value) }/>
+      <div className="ask-form-content">
+        <div className="form-group">
+          <label htmlFor="title">
+            <strong>Title</strong>
+            <small>Be specific and imagine you’re asking a question to another person</small>
+          </label>
+          <input required id="title" type="text" value={ title } onChange={ event => setTitle(event.target.value) }/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="body">
+            <strong>Body</strong>
+            <small>Include all the information someone would need to answer your question</small>
+          </label>
+          <textarea required id="body" value={ body } onChange={ event => setBody(event.target.value) }/>
+          <EditorHint />
+        </div>
+        <ReactMarkdown source={body} />
       </div>
       <button className="button button-primary">Submit</button>
     </form>
---
+++ frontend/components/post/widgets.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const EditorHint = () => <div className="editor-hint">
+  <span>```<code>code</code>```</span>
+  <span>**<strong>bold</strong>**</span>
+  <span>*<em>italic</em>*</span>
+  <span>>quote</span>
+</div>
