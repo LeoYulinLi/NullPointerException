4e263fc1fd697856a82daa0b34660d499bf1becf
LeoYulinLi <LeoYulinLi@gmail.com>
2020-06-27 22:34:19 -0700

    Blocked non-logged-in user from answering questions

--- frontend/components/post/answer_form.jsx
+++ frontend/components/post/answer_form.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { answerQuestion, askQuestion, getQuestionThread } from "../../actions/post_actions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ReactMarkdown from "react-markdown";
 import { FormBodyEditor } from "./widgets";
 import { useHistory } from "react-router";
+import { isLoggedInSelector } from "../../selectors/selectors";
+import { Link } from "react-router-dom";
 
 /**
  * @param {number} id
@@ -14,19 +16,27 @@ const AnswerForm = ({ id }) => {
 
   const dispatch = useDispatch();
 
+  const loggedIn = useSelector(isLoggedInSelector);
+
   function handleSubmit(event) {
     event.preventDefault();
     dispatch(answerQuestion(id, { body }))
       .then(() => dispatch(getQuestionThread(id)));
   }
 
-  return <div className="post-page">
+  const form = <div className="post-page">
     <h2>Your Answer</h2>
     <form onSubmit={ handleSubmit }>
       <FormBodyEditor body={ body } setBody={ setBody } rows={ 10 }/>
       <button className="button button-primary">Submit</button>
     </form>
   </div>
+
+  const pleaseLoginMessage = <div className="post-page">
+    <h2>You must <Link to="/login" className="primary">log in</Link> to answer this question.</h2>
+  </div>
+
+  return loggedIn ? form : pleaseLoginMessage
 
 }
 
