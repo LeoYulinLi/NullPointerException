a43001a6fe705387693f3113cff7e50bc0cfd45e
LeoYulinLi <LeoYulinLi@gmail.com>
2020-07-01 15:39:32 -0700

    Initial work on home page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,6 +14,7 @@ import Thread from "./post/thread";
 import EditForm from "./post/edit_form";
 import SearchResult from "./search";
 import UserInfo from "./user/user_info";
+import HomePage from "./home";
 
 const App = () => {
 
@@ -28,23 +29,28 @@ const App = () => {
 
   return <HashRouter>
     <MainNav/>
-    <div className="main">
-      <Switch>
-        <Route path="/" exact>
-          { isLoggedIn ? <QuestionsIndex/> : <h1>Hello, Guest</h1> }
-        </Route>
-        <Route path="/questions" exact component={ QuestionsIndex }/>
-        <Route path="/questions/:id" exact component={ Thread }/>
-        <Route path="/search" exact component={ SearchResult }/>
-        <Route path="/search/:query" exact component={ SearchResult }/>
-        <Route path="/users/:id" exact component={ UserInfo }/>
-        <AuthRoute path="/signup" component={ Signup }/>
-        <AuthRoute path="/login" component={ Login }/>
-        <ProtectedRoute path="/ask" component={ AskForm }/>
-        <ProtectedRoute path="/posts/:id/edit" exact component={ EditForm }/>
-        <Route path="/"><h1>Nothing to See Here owo</h1></Route>
-      </Switch>
-    </div>
+    <Switch>
+      <AuthRoute path="/" exact component={ HomePage }/>
+      <Route path="/">
+        <div className="main">
+          <Switch>
+            <Route path="/" exact>
+              { isLoggedIn && <QuestionsIndex/> }
+            </Route>
+            <Route path="/questions" exact component={ QuestionsIndex }/>
+            <Route path="/questions/:id" exact component={ Thread }/>
+            <Route path="/search" exact component={ SearchResult }/>
+            <Route path="/search/:query" exact component={ SearchResult }/>
+            <Route path="/users/:id" exact component={ UserInfo }/>
+            <AuthRoute path="/signup" component={ Signup }/>
+            <AuthRoute path="/login" component={ Login }/>
+            <ProtectedRoute path="/ask" component={ AskForm }/>
+            <ProtectedRoute path="/posts/:id/edit" exact component={ EditForm }/>
+            <Route path="/"><h1>Nothing to See Here owo</h1></Route>
+          </Switch>
+        </div>
+      </Route>
+    </Switch>
   </HashRouter>
 }
 
---
+++ frontend/components/home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return <div className="home">
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Null Pointer Exception</h1>
+        <p>This is a thing. A thing that you can do a thing with a thing. Thingy thing.</p>
+        <p>This background color hurts my eyes.</p>
+        <p>And I almost never use this page when I visit Stack Overflow anyway...</p>
+        <Link to="/questions" className="button button-success">BUTTON!</Link>
+      </div>
+    </div>
+  </div>
+}
+
+export default HomePage;
