f111b5f2b00890b35549fc47ece86dc995becde4
LeoYulinLi <LeoYulinLi@gmail.com>
2020-07-28 16:02:58 -0700

    Added about on splash page

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -8,10 +8,34 @@ const HomePage = () => {
         <h1>Null Pointer Exception</h1>
         <p>We build products that empower developers and connect them to solutions that enable productivity,
           growth, and discovery.</p>
-        <object className="rails-logo" />
-        <object className="react-logo" />
-        <object className="postgresql-logo" />
         <Link to="/questions" className="button button-success">See All Questions</Link>
+      </div>
+    </div>
+    <div className="about">
+      <h1>Technology used</h1>
+      <div className="about-container">
+        <div className="about-item">
+          <object className="react-logo" />
+          <h3>Frontend</h3>
+          The front end was built on React + Redux to provide a responsive experience. Some extra details, such as
+          loading indicator, form validation indicator, and form disabling on submitting were added using a combination
+          of JavaScript and SCSS to improve user experience. React also allowed the reuse of many components, saving
+          time and reducing errors.
+        </div>
+        <div className="about-item">
+          <object className="rails-logo" />
+          <h3>Backend</h3>
+          Backend is a Rest API server implemented using Ruby on Rails. Most of the heavy lifting is done on the
+          backend. It handles user auth, CRUD requests on questions, answers, posts, votes, and more. It also has a
+          simple text search feature that searches for both the title and the body of all posts.
+        </div>
+        <div className="about-item">
+          <object className="postgresql-logo" />
+          <h3>Database</h3>
+          This application uses PostgreSQL. Most queries are handled by Rails Active Record, but when querying data such
+          as the current revisions of a question or an answer, SQL View and Rails Scenic were used to simplify active
+          record querying statements.
+        </div>
       </div>
     </div>
   </div>
