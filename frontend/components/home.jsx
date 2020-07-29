3fdc31bc00edd0a37a475c8ebdbcf682e25c8b98
LeoYulinLi <LeoYulinLi@gmail.com>
2020-07-29 08:54:43 -0700

    Fixed a typo

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
           <object className="postgresql-logo" />
           <h3>Database</h3>
           This application uses PostgreSQL. Most queries are handled by Rails Active Record, but when querying data such
-          as the current revisions of a question or an answer, SQL views and Rails Scenic were used to simplify active
+          as the current revision of a question or an answer, SQL views and Rails Scenic were used to simplify active
           record querying statements.
         </div>
       </div>
